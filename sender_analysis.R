a387f439471365024e47444575463616f33bd0c8
Sam Lindquist <samlindj@gmail.com>
2023-09-13 19:46:02 -0700

    new stuff

---
+++ sender_analysis.R
@@ -0,0 +1,401 @@
+# this code does some exploratory analysis on the sender data
+
+library(tidyverse)
+library(Hmisc)
+library(DescTools)
+library(stargazer)
+
+#path_to_dir <- "/Users/samuellindquist/Dropbox/expert_bias/code_data" # sams mac
+path_to_dir <- "C:/Users/slindquist/Dropbox/expert_bias/code_data" # sams windows
+
+setwd(path_to_dir)
+
+df <- read_csv("output/sender_clean.csv")
+
+# getting data ready for analysis
+treatments <- c(
+  "Biased Down Expert" = "bde", 
+  "Non-biased Expert" = "nbe",
+  "Biased Up Expert" = "bue",
+  "Biased Down Layperson" = "bdl",
+  "Non-biased Layperson" = "nbl",
+  "Biased Up Layperson" = "bul"
+)
+
+# trying measure of accuracy which is inverse distance: the higher the more accurate
+df$inverse_post_inflation_accuracy <- ifelse(df$post_inflation_accuracy != 0, abs(1/df$post_inflation_accuracy), NA)
+df$inverse_post_inflation_accuracy <- 1/(1+df$post_inflation_accuracy^2)
+
+df$inverse_prior_inflation_accuracy <- 1/(1+df$prior_inflation_accuracy^2)
+
+#### graphs ####
+# mean inflation posterior
+df %>% 
+  group_by(treatment) %>% 
+  mutate(post_inflation_point_sd = sd(post_inflation_point, na.rm = TRUE),
+         post_inflation_point_mean = mean(post_inflation_point, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  ggplot(color = treatment) +
+  geom_bar(aes(x = treatment, 
+               y = post_inflation_point),
+           stat = 'summary', 
+           fun = 'mean') + 
+  scale_x_discrete(breaks = treatments,
+                   labels = names(treatments)) +
+  geom_errorbar(aes(x = treatment,
+                    ymin = post_inflation_point_mean - post_inflation_point_sd,
+                    ymax = post_inflation_point_mean + post_inflation_point_sd)) +
+  theme(axis.text.x = element_text(angle = 45))
+
+# median inflation posterior
+df %>% 
+  group_by(treatment) %>% 
+  mutate(post_inflation_point_sd = sd(post_inflation_point, na.rm = TRUE),
+         post_inflation_point_median = median(post_inflation_point, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_bar(aes(x = treatment, 
+               y = post_inflation_point,
+               fill = treatment),
+           stat = 'summary', 
+           fun = 'median') + 
+  geom_errorbar(aes(x = treatment,
+                    ymin = post_inflation_point_median - post_inflation_point_sd,
+                    ymax = post_inflation_point_median + post_inflation_point_sd))
+
+# median inflation updating 
+df %>% 
+  group_by(treatment) %>% 
+  mutate(inflation_update_sd = sd(inflation_update, na.rm = TRUE),
+         inflation_update_median = median(inflation_update, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_bar(aes(x = treatment, 
+               y = inflation_update_median,
+               fill = treatment),
+           stat = 'summary', 
+           fun = 'median') + 
+  geom_errorbar(aes(x = treatment,
+                    ymin = inflation_update_median - inflation_update_sd,
+                    ymax = inflation_update_median + inflation_update_sd))
+
+# mean inflation updating 
+df %>% 
+  group_by(treatment) %>% 
+  mutate(inflation_update_sd = sd(inflation_update, na.rm = TRUE),
+         inflation_update_mean = mean(inflation_update, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_bar(aes(x = treatment, 
+               y = inflation_update_mean,
+               fill = treatment),
+           stat = 'summary', 
+           fun = 'mean') + 
+  geom_errorbar(aes(x = treatment,
+                    ymin = inflation_update_mean - inflation_update_sd,
+                    ymax = inflation_update_mean + inflation_update_sd))
+
+# median inflation accuracy 
+df %>% 
+  group_by(treatment) %>% 
+  mutate(inflation_accuracy_sd = sd(post_inflation_accuracy, na.rm = TRUE),
+         inflation_accuracy_median = median(post_inflation_accuracy, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_bar(aes(x = treatment, 
+               y = inflation_accuracy_median,
+               fill = treatment),
+           stat = 'summary', 
+           fun = 'median') + 
+  geom_errorbar(aes(x = treatment,
+                    ymin = inflation_accuracy_median - inflation_accuracy_sd,
+                    ymax = inflation_accuracy_median + inflation_accuracy_sd))
+
+# mean inflation accuracy 
+df %>% 
+  group_by(treatment) %>% 
+  mutate(inflation_accuracy_sd = sd(post_inflation_accuracy, na.rm = TRUE),
+         inflation_accuracy_mean = mean(post_inflation_accuracy, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_bar(aes(x = treatment, 
+               y = inflation_accuracy_mean,
+               fill = treatment),
+           stat = 'summary', 
+           fun = 'mean') + 
+  geom_errorbar(aes(x = treatment,
+                    ymin = inflation_accuracy_mean - inflation_accuracy_sd,
+                    ymax = inflation_accuracy_mean + inflation_accuracy_sd))
+
+
+#### regressions ####
+reg <- function(x) {
+  summary(lm(x, df))
+}
+
+
+# compared to biased down expert, everybody else has a lower inflation update
+reg(inflation_update ~ treatment + prior_inflation_density_sd + prior_inflation_point + gender + political_party + employed + family_yearly_income)
+model1 <- reg(inflation_update ~ treatment + prior_inflation_density_sd + prior_inflation_point)
+
+
+model1$coefficients
+
+reg(post_inflation_accuracy ~ treatment + prior_inflation_accuracy + gender + political_party + employed + family_yearly_income)
+reg(post_inflation_accuracy ~ info_treat + bias_treat + info_treat*bias_treat + prior_inflation_accuracy + prior_inflation_density_sd + gender + political_party + employed + family_yearly_income)
+
+
+
+t.test(inflation_update ~ info_treat, data = df)
+t.test(inflation_update ~ bias_treat, data = df %>% filter(bias_treat != "Non-biased"))
+t.test(inflation_update ~ bias_treat, data = df %>% filter(bias_treat != "Biased Down"))
+t.test(inflation_update ~ bias_treat, data = df %>% filter(bias_treat != "Biased Up"))
+
+t.test(inflation_update ~ bias_treat, data = df %>% filter(bias_treat != "Biased Up"))
+
+# fixing bias level, looking at 
+t.test(inflation_update ~ bias_treat, data = df)
+
+# it appears overall that the bias treatment moved people more than the information treatment
+
+# one prediction: holding bias fixed, experts will be more accurate than nonexperts
+t.test(post_inflation_accuracy ~ info_treat, data = df %>% filter(bias_treat == "Non-biased"))
+t.test(post_inflation_accuracy ~ info_treat, data = df %>% filter(bias_treat == "Biased Up"))
+t.test(post_inflation_accuracy ~ info_treat, data = df %>% filter(bias_treat == "Biased Down"))
+
+# another prediction: holding expertise fixed, non-biased people will be the most accurate, biased down people will underreport, biased up people will over report
+t.test(post_inflation_accuracy ~ bias_treat, data = df %>% filter(info_treat == "Expert" & bias_treat != "Non-biased"))
+t.test(post_inflation_accuracy ~ bias_treat, data = df %>% filter(info_treat == "Expert" & bias_treat != "Biased Up"))
+t.test(post_inflation_accuracy ~ bias_treat, data = df %>% filter(info_treat == "Expert" & bias_treat != "Biased Down"))
+
+
+# separating out info and bias 
+df$info_treat <- relevel(as.factor(df$info_treat), ref = "Expert")
+df$bias_treat <- relevel(as.factor(df$bias_treat), ref = "Biased Down")
+
+#inflation
+reg(post_inflation_accuracy ~ info_treat + bias_treat + info_treat*bias_treat + prior_inflation_accuracy + prior_inflation_density_sd + gender + political_party + employed + family_yearly_income)
+reg(inflation_update ~ info_treat+ bias_treat + info_treat*bias_treat + prior_inflation_density_sd + gender + political_party + employed + family_yearly_income)
+
+#pop
+reg(post_ucsd_pop_accuracy ~ info_treat + bias_treat + info_treat*bias_treat + prior_ucsd_pop_accuracy + prior_ucsd_pop_density_sd + gender + political_party + employed + family_yearly_income)
+reg(ucsd_pop_update ~ info_treat + bias_treat + info_treat*bias_treat + prior_ucsd_pop_density_sd + gender + political_party + employed + family_yearly_income)
+
+reg(post_ucsd_pop_accuracy ~ treatment + prior_ucsd_pop_accuracy + prior_ucsd_pop_density_sd + gender + political_party + employed + family_yearly_income)
+reg(ucsd_pop_update ~ treatment + prior_ucsd_pop_density_sd + gender + political_party + employed + family_yearly_income)
+
+# ucsd rank
+reg(post_ucsd_rank_accuracy ~ info_treat + bias_treat + info_treat*bias_treat + prior_ucsd_rank_accuracy + ucsd_rank_prior_density_sd + gender + political_party + employed + family_yearly_income)
+reg(ucsd_rank_update ~ info_treat + bias_treat + info_treat*bias_treat + ucsd_rank_prior_density_sd + gender + political_party + employed + family_yearly_income)
+
+reg(post_ucsd_rank_accuracy ~ treatment + prior_ucsd_rank_accuracy + ucsd_rank_prior_density_sd + gender + political_party + employed + family_yearly_income)
+reg(ucsd_rank_update ~ treatment + ucsd_rank_prior_density_sd + gender + political_party + employed + family_yearly_income)
+
+
+reg(inflation_update ~ info_treat+ bias_treat + info_treat*bias_treat + prior_inflation_density_sd + gender + political_party + employed + family_yearly_income)
+reg(inflation_update ~ info_treat + bias_treat + info_treat*bias_treat + gender + political_party + employed + family_yearly_income)
+
+reg(prior_inflation_point ~ info_treat)
+reg(prior_inflation_point ~ bias_treat)
+
+# layperson has significantly higher inflation prior than expert
+reg(post_inflation_point ~ info_treat)
+
+# incentives insignificant
+reg(post_inflation_point ~ bias_treat)
+
+reg(inflation_update ~ info_treat)
+reg(inflation_update ~ bias_treat)
+
+# adding controls
+# layperson still has higher inflation priors with controls
+reg(post_inflation_point ~ info_treat + gender + political_party + employed + family_yearly_income)
+reg(post_inflation_point ~ bias_treat + gender + political_party + employed + family_yearly_income)
+
+reg(inflation_update ~ info_treat + gender + political_party + employed + family_yearly_income)
+
+# biased up treatment marginally significant downwards
+reg(inflation_update ~ bias_treat + gender + political_party + employed + family_yearly_income)
+
+# looking at the other variables, ucsd stats and population
+reg(ucsd_pop_update ~ info_treat + gender + political_party + employed + family_yearly_income)
+reg(ucsd_pop_update ~ bias_treat + gender + political_party + employed + family_yearly_income)
+
+reg(ucsd_rank_update ~ info_treat + gender + political_party + employed + family_yearly_income)
+reg(ucsd_rank_update ~ bias_treat + gender + political_party + employed + family_yearly_income)
+
+# looking at accuracy
+reg(post_inflation_accuracy ~ bias_treat)
+reg(post_ucsd_pop_accuracy ~ bias_treat)
+reg(post_ucsd_rank_accuracy ~ bias_treat)
+
+reg(post_inflation_accuracy ~ info_treat)
+reg(post_ucsd_pop_accuracy ~ info_treat)
+reg(post_ucsd_rank_accuracy ~ info_treat) # info makes people more accurate
+
+reg(post_inflation_accuracy ~ treatment + gender + political_party + employed + family_yearly_income)
+reg(post_inflation_accuracy_abs ~ treatment + gender + political_party + employed + family_yearly_income)
+
+
+
+
+#### summary stats ####
+df %>% group_by(treatment) %>% 
+  summarise(mean = mean(post_inflation_accuracy, na.rm = T),
+            sd = sd(post_inflation_accuracy, na.rm = T)) %>% 
+  arrange(mean)
+
+df %>% group_by(treatment) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+df %>% group_by(info_treat) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+# for the experts, we have the bd < nb < bu for inflation posterior
+df %>% 
+  filter(info_treat == "Expert") %>% 
+  group_by(bias_treat) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+# this pattern does not hold for laypeople, we get nb < bd < bu
+df %>% 
+  filter(info_treat == "Layperson") %>% 
+  group_by(bias_treat) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+# comparing two non-biased people, the expert is more accurate
+df %>% 
+  filter(bias_treat == "Non-biased") %>% 
+  group_by(info_treat) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+# comparing two biased up people, the expert has a higher estimate
+df %>% 
+  filter(bias_treat == "Biased Up") %>% 
+  group_by(info_treat) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+# comparing two biased down people, the expert has a lower estimate
+df %>% 
+  filter(bias_treat == "Biased Down") %>% 
+  group_by(info_treat) %>% 
+  summarise(mean = mean(post_inflation_point, na.rm = T),
+            sd = sd(post_inflation_point, na.rm = T)) %>% 
+  arrange(mean)
+
+
+
+post_vars <- c("post_inflation_point", "post_ucsd_rank", "post_ucsd_pop")
+post_vars <- c("post_inflation_accuracy", "post_ucsd_rank_accuracy", "post_ucsd_pop_accuracy")
+
+
+for (i in seq_along(post_vars)) {
+  print(paste("-----------", post_vars[i], "------------"))
+  
+  print("#### For experts ####")
+  df %>% 
+    filter(info_treat == "Expert") %>% 
+    group_by(bias_treat) %>% 
+    summarise(median = median(get(post_vars[i]), na.rm = T),
+              sd = sd(get(post_vars[i]), na.rm = T)) %>% 
+    arrange(median) %>% 
+    print()
+  
+  print("#### For lay people ####")
+  df %>% 
+    filter(info_treat == "Layperson") %>% 
+    group_by(bias_treat) %>% 
+    summarise(median = median(get(post_vars[i]), na.rm = T),
+              sd = sd(get(post_vars[i]), na.rm = T)) %>% 
+    arrange(median) %>% 
+    print()
+  
+  print("#### non biased people ####")
+  df %>% 
+    filter(bias_treat == "Non-biased") %>% 
+    group_by(info_treat) %>% 
+    summarise(median = median(get(post_vars[i]), na.rm = T),
+              sd = sd(get(post_vars[i]), na.rm = T)) %>% 
+    arrange(median) %>% 
+    print()
+  
+  print("#### biased up ####")
+  df %>% 
+    filter(bias_treat == "Biased Up") %>% 
+    group_by(info_treat) %>% 
+    summarise(median = median(get(post_vars[i]), na.rm = T),
+              sd = sd(get(post_vars[i]), na.rm = T)) %>% 
+    arrange(median) %>% 
+    print()
+  
+  print("#### biased down ####")
+  df %>% 
+    filter(bias_treat == "Biased Down") %>% 
+    group_by(info_treat) %>% 
+    summarise(median = median(get(post_vars[i]), na.rm = T),
+              sd = sd(get(post_vars[i]), na.rm = T)) %>% 
+    arrange(median) %>% 
+    print()
+}
+
+
+
+
+
+
+
+
+reg(inflation_update ~ treatment + gender + political_party + employed + family_yearly_income)
+reg(inflation_update ~ treatment)
+
+describe(df$prior_inflation_point)
+sd(df$prior_inflation_point, na.rm = T)
+
+describe(df$post_inflation_point)
+sd(df$post_inflation_point, na.rm = T)
+
+df %>% group_by(info_treat) %>% 
+  summarise(sd(prior_inflation_point, na.rm= T),
+            sd(post_inflation_point, na.rm= T)) %>% 
+  arrange()
+
+
+
+## please comment on this script 
+
+# looking at what values we can send to recipients
+# inflation: 1, 2, 4, 5, 10, 25
+df %>% group_by(post_inflation_point) %>% 
+  distinct(treatment) %>% 
+  count() %>% 
+  filter(n == 6)
+
+# ucsd rank: 32, 33, 34, 35
+df %>% group_by(post_ucsd_rank) %>% 
+  distinct(treatment) %>% 
+  count() %>% 
+  filter(n == 6)
+
+# ucsd pop: 30,000, 40,000, 45,000
+df %>% group_by(post_ucsd_pop) %>% 
+  distinct(treatment) %>% 
+  count() %>% 
+  filter(n == 6)
+
+
+
+
+
+
---
+++ sender_clean.R
@@ -0,0 +1,166 @@
+library(tidyverse)
+library(Hmisc)
+library(DescTools)
+library(tidylog, warn.conflicts = FALSE)
+
+#path_to_dir <- "/Users/samuellindquist/Dropbox/expert_bias/code_data" # sams mac
+path_to_dir <- "C:/Users/slindquist/Dropbox/expert_bias/code_data" # sams windows
+
+setwd(path_to_dir)
+
+df <- read_csv("input/sender_raw.csv")
+
+# deleting first two rows: qualtrics metadata
+df <- df %>% 
+  slice(3:nrow(df))
+
+# deleting attention check failures (also deletes tests)
+df <- df %>%
+  filter(attention_check_1 == 100 & attention_check_2 == 50)
+
+# deleting those who didn't finish
+df <- df %>% 
+  filter(Finished %in% "True")
+
+# deleting second responses of people that did survey twice
+df$sona_id <- ifelse(is.na(df$sona_id), df$Q89, df$sona_id)
+
+df <- df %>% 
+  rename(id = Q89) %>% 
+  mutate(id = ifelse(is.na(id), sona_id, id))
+
+df <- df %>% 
+  filter(!is.na(id)) %>% 
+  distinct(id, .keep_all = TRUE)
+
+# using 'id' variable as identifier because question forced individuals to give a five digit response so its more regular
+
+# renaming
+df <- df %>% 
+  rename(prior_inflation_point = prior_inflation_poin,
+         ucsd_rank_prior_point = ucsd_rank_prior_poin)
+
+# converting some vars to numeric
+df <- df %>% 
+  mutate(across(attention_check_1:post_ucsd_pop, as.numeric))
+
+# trying with data winsorized
+df$post_inflation_point <- Winsorize(df$post_inflation_point, probs = c(0.05, 0.95))
+df$post_ucsd_pop <- Winsorize(df$post_inflation_point, probs = c(0.05, 0.95))
+df$post_ucsd_rank <- Winsorize(df$post_inflation_point, probs = c(0.05, 0.95))
+
+# breaking up treatments along expert/layperson and accurate/upbiased/downbiased dims
+df <- df %>% 
+  mutate(info_treat = case_when(grepl("(nbl|bul|bdl)", treatment) ~ "Layperson",
+                                grepl("(bde|bue|nbe)", treatment) ~ "Expert"),
+         bias_treat = case_when(grepl("(bue|bul)", treatment) ~ "Biased Up",
+                                grepl("(nbe|nbl)", treatment) ~ "Non-biased",
+                                grepl("(bde|bdl)", treatment) ~ "Biased Down"))
+
+# looking at revision of information after the treatment
+df$inflation_update <- df$prior_inflation_point - df$post_inflation_point
+df$ucsd_pop_update <- df$prior_ucsd_pop_point - df$post_ucsd_pop
+df$ucsd_rank_update <- df$ucsd_rank_prior_point - df$post_ucsd_rank
+
+# accuracy outcomes 
+correct_inflation_forecast_march_23 <- 3.2 # "correct" inflation rate (as forecasted by the fed)
+correct_inflation_forecast_june_23 <- 3.3
+
+# correct ucsd population 
+correct_ucsd_pop <- 42968 # for fall of academic year 2022-2023
+
+#correct ucsd rank
+correct_ucsd_rank <- 34 # for academic year 2022-2023
+
+# accuracy measures
+df$post_inflation_accuracy <- df$post_inflation_point - correct_inflation_forecast_march_23
+df$post_ucsd_pop_accuracy <- df$post_ucsd_pop - correct_ucsd_pop
+df$post_ucsd_rank_accuracy <- df$post_ucsd_rank - correct_ucsd_rank
+
+df$prior_inflation_accuracy <- df$prior_inflation_point - correct_inflation_forecast_march_23
+df$prior_ucsd_pop_accuracy <- df$prior_ucsd_pop_point - correct_ucsd_pop
+df$prior_ucsd_rank_accuracy <- df$ucsd_rank_prior_point - correct_ucsd_rank
+
+# distribution of probabilities for each measure (inflation, pop, rank)
+
+# inflation
+inflation_buckets <- tibble(lower_bound = c(-Inf, -12,  -8,  -4,  -2, 0,  2, 4, 8, 12),
+                            upper_bound = c(-12, -8, -4, -2, 0, 2, 4, 8, 12, Inf))
+
+inflation_buckets$variable <- names(df)[grepl("prior_inflation_dist", names(df))]
+
+
+# ucsd ranking
+rank_buckets <- tibble(lower_bound = c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60),
+                       upper_bound = c(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, Inf))
+
+rank_buckets$variable <- names(df)[grepl("ucsd_rank_prior_dist", names(df))]
+
+# ucsd pop
+pop_buckets <- tibble(lower_bound = c(0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000),
+                      upper_bound = c(5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000, Inf))
+
+pop_buckets$variable <- names(df)[grepl("prior_ucsd_pop_dist", names(df))]
+
+# appending these three variable types together
+buckets <- rbind(inflation_buckets, pop_buckets, rank_buckets)
+
+buckets$variable_name <- str_split_fixed(buckets$variable, "_dist_", 2)[, 1]
+buckets$variable_num <- as.numeric(str_split_fixed(buckets$variable, "_dist_", 2)[, 2])
+
+buckets <- buckets %>% 
+  mutate(bucket_avg = ifelse(is.infinite(lower_bound), upper_bound + (upper_bound - lead(upper_bound))/2, 
+                                       ifelse(is.infinite(upper_bound), lower_bound + (lower_bound - lag(lower_bound))/2,
+                                              (lower_bound + upper_bound)/2)))
+
+df_dist <- df %>% 
+  select(id, 
+         starts_with("prior_inflation_dist"), 
+         starts_with("ucsd_rank_prior_dist"), 
+         starts_with("prior_ucsd_pop_dist")) %>% 
+  pivot_longer(cols = !id,
+               names_to = "variable",
+               values_to = "probability") %>% 
+  mutate(probability = probability)
+
+df_dist <- left_join(df_dist, buckets, by = "variable") %>% 
+  arrange(id, variable_name, variable_num)
+
+# making sure all probabilities sum to 100
+df_dist <- df_dist %>% 
+  group_by(id, variable_name) %>% 
+  mutate(prob_sum = round(sum(probability))) %>% 
+  ungroup() %>% 
+  mutate(prob_clean = ifelse(prob_sum == 100, probability, NA))
+
+# dropping responses from anybody who gave negative responses
+df_dist <- df_dist %>% 
+  group_by(id, variable_name) %>% 
+  mutate(any_prob_less_zero = ifelse(any(probability < 0), TRUE, FALSE)) %>% 
+  ungroup() %>% 
+  mutate(prob_clean = ifelse(any_prob_less_zero == TRUE, NA, prob_clean)) %>% 
+  ungroup()
+
+# vars no longer needed
+df_dist <- df_dist %>% 
+  select(-c(prob_sum, any_prob_less_zero))
+
+df_dist_agg <- df_dist %>% 
+  group_by(id, variable_name) %>% 
+  summarise(density_mean = sum(bucket_avg*prob_clean),
+         density_sd = sqrt(sum(prob_clean*(bucket_avg - density_mean)^2))) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = "variable_name",
+              values_from = c("density_mean", "density_sd"),
+              names_glue = "{variable_name}_{.value}")
+
+# merging back into dataset
+df <- df %>% 
+  left_join(df_dist_agg, by = "id")
+
+# exporting            
+write_csv(df, "output/sender_clean.csv")
+
+
+
+
